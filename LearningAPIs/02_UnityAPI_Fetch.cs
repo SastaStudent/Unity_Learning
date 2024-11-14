7decef4e6ff3f459f82dcd41dabbc3d2a3cd0120
SastaStudent <mc1377929@gmail.com>
2024-11-14 15:17:01 +0530

    Get using Coroutine

--- LearningAPIs/02_UnityAPI_Fetch.cs
+++ LearningAPIs/02_UnityAPI_Fetch.cs
@@ -1,70 +1,92 @@
-using UnityEngine;
-using System.Collections;
-using UnityEngine.Networking;
-using System.Threading.Tasks;
+# There are two method of Calling Api in Unity
+## 1. Call API using Coroutine
 
-public class Fetch : MonoBehaviour
-{    
-
-    void Start()
+### 1. For Getting  String response/Json
+```csharp
+private IEnumerator GetReq(string url)
+{
+    using (UnityWebRequest request = UnityWebRequest.Get(url))
     {
-        string url = "https://api.github.com/users/sastastudent";
-        Debug.Log("Requesting: " + url);
+        // Send the request and wait for the response
+        yield return request.SendWebRequest();
 
-        // Call the asynchronous method to get the data
-        StartCoroutine(GetReq(url));
-
-    }
-    public IEnumerator GetReq(string url)
-    {
-        using (UnityWebRequest request = UnityWebRequest.Get(url))
+        // Check for errors
+        if (request.result == UnityWebRequest.Result.Success)
         {
-            // Send the request and wait for the response
-            yield return request.SendWebRequest();
+            // If the request was successful, parse the response
+            //(parsing our request to string)
+            string jsonResponse = request.downloadHandler.text;
+            // Debug.Log("Response : " + jsonResponse);
 
-            // Check for errors
-            if (request.result == UnityWebRequest.Result.Success)
-            {
-                // If the request was successful, parse the response
-                string jsonResponse = request.downloadHandler.text;
-                // Debug.Log("Response : " + jsonResponse);
+            // Deserialize the JSON response to a ResponseClass object
+            // (Converting data from string to Json) => which leads to an object
+            ResponseClass user = JsonUtility.FromJson<ResponseClass>(jsonResponse);
+            // here we can store this user data in Database for future use.
 
-                // Deserialize the JSON response to a ResponseClass object
-                ResponseClass user = JsonUtility.FromJson<ResponseClass>(jsonResponse);
-
-                // Use the response (e.g., print it to the console)
-                // Debug.Log("User Data " + user);
-                Debug.Log("User Login: "+ user.login);
-                Debug.Log("User Name: "+ user.name);
+            //checking data after fetching
+            Debug.Log("User Login: "+ user.login);
+            Debug.Log("User Name: "+ user.name);
+            Debug.Log("User Bio: "+ user.bio);
+            Name.text = user.name;
+            followers.text = user.followers.ToString();
+            followings.text = user.following.ToString();
+            txt.text = user.bio;
+            // image.sprite = user.avatar_url;
+            StartCoroutine(GetAvatar(user.avatar_url));
                  
-            }
-            else
-            {
-                // Handle errors (e.g., network issues or bad status code)
-                Debug.Log("Error");
-                Debug.LogError($"Request failed: {request.error}");
-            }
+        }
+        else
+        {
+            // Handle errors (e.g., network issues or bad status code)
+            input.text="";
+            Debug.Log("Error");
+            Debug.LogError($"Request failed: {request.error}");
+                 
         }
     }
-
-    // public ResponseClass GetReq(string url)
-    // {
-    //     try
-    //     {
-    //         HttpWebRequest req =(HttpWebRequest) WebRequest.Create(url);
-    //         HttpWebResponse res = (HttpWebResponse) req.GetResponse();
-    //         StreamReader reader = new StreamReader(res.GetResponseStream());
-    //         string json = reader.ReadToEnd();
-    //         return JsonUtility.FromJson<ResponseClass>(json);
-    //     }
-    //     catch (WebException ex)
-    //     {
-    //         Debug.Log("Exception Error");
-    //         throw ex;
-    //     }
-    // }
-
-     
 }
+```
+### 2. For getting Images
+```csharp
+private IEnumerator GetAvatar(string url)
+{
+    // First, make a request (for example, you may be checking for some API response or status)
+    using (UnityWebRequest getRequest = UnityWebRequest.Get(url))  
+    {
+        yield return getRequest.SendWebRequest();
 
+        if (getRequest.result == UnityWebRequest.Result.Success)
+        {
+            // Now, make the request to download the texture (image)
+            using (UnityWebRequest textureRequest = UnityWebRequestTexture.GetTexture(url))  
+            {
+                // Wait for the request to finish
+                yield return textureRequest.SendWebRequest();
+
+                if (textureRequest.result == UnityWebRequest.Result.Success)
+                {
+                    // Get the downloaded texture from the request
+                    Texture2D texture = ((DownloadHandlerTexture)textureRequest.downloadHandler).texture;
+
+                    // Create a sprite from the downloaded texture
+                    Sprite sprite = Sprite.Create(texture, new Rect(0, 0, texture.width, texture.height), new Vector2(0.5f, 0.5f));
+
+                    // Assign the sprite to the Image component
+                    image.sprite = sprite;
+                }
+                else
+                {
+                    // If there's an error in the texture request, print the error message
+                    Debug.LogError("Failed to load avatar image: " + textureRequest.error);
+                }
+            }
+        }
+        else
+        {
+            // If there's an error in the initial get request (e.g., API call), print the error message
+            Debug.LogError("Failed to get URL: " + getRequest.error);
+        }
+    }
+}
+```
  
